24c0067517f00e672c3d8e7dee2b656eaf682e83
Your Name <you@example.com>
2024-07-15 11:42:17 -0300

    feat: add my trips route

---
+++ src/http/routes/trips/get-my-trips.ts
@@ -0,0 +1,32 @@
+import type { FastifyInstance } from 'fastify'
+import type { ZodTypeProvider } from 'fastify-type-provider-zod'
+
+import { prisma } from '@/common/prisma/prisma'
+import { auth } from '@/http/middlewares/auth.middleware'
+
+export async function getMyTrips(app: FastifyInstance) {
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .register(auth)
+    .get(
+      '/trips/me',
+      {
+        schema: {},
+      },
+      async (request) => {
+        const userId = await request.getCurrentUserId()
+
+        const trips = await prisma.trip.findMany({
+          where: {
+            participants: {
+              some: {
+                user_id: userId,
+              },
+            },
+          },
+        })
+
+        return { trips }
+      },
+    )
+}
